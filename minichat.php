18a69d895295377750e84820b39a782b787fa43e
Ahmed <hitmi6511@gmail.com>
2016-10-11 15:10:04 +0200

    create minichat

--- minichat.php
+++ minichat.php
@@ -2,12 +2,12 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <title>Titre de ma page</title>
+        <title>Minichat</title>
     </head>
     <body>
        <form action="minichat_post.php" method="post">
            <p>
-           <label for="pseudo">Pseudo</label> : <input type="text" name="pseudo" id="pseudo" /><br />
+           <label for="nom">Pseudo</label> : <input type="text" name="nom" id="nom" /><br />
            <label for="message">Message</label> :  <input type="text" name="message" id="message" /><br />
            <input type="submit" value="Envoyer" />
           </p>
@@ -15,23 +15,20 @@
        <?php
        try
        {
-       	// connexion bdd minichat
            $bdd = new PDO('mysql:host=localhost;dbname=minichat;charset=utf8', 'root', 'melissa2000');
        }
        catch(Exception $e)
        {
-       	// En cas d'erreur, on affiche un message et on arrête tout
                die('Erreur : '.$e->getMessage());
 
        }
-       $reponse = $bdd->query('SELECT pseudo, message FROM minichat ORDER BY ID DESC LIMIT 0, 10');
+       $reponse = $bdd->query('SELECT nom, message FROM minichat ORDER BY ID DESC LIMIT 0, 10');
+
+
        while ($donnees = $reponse->fetch())
        {
-           echo '<p><strong>' . htmlspecialchars($donnees['pseudo']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>';
+           echo '<p><strong>' . htmlspecialchars($donnees['nom']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>';
        }
-
-
-
-
+       ?>
     </body>
 </html>
--- minichat_post.php
+++ minichat_post.php
@@ -2,12 +2,35 @@
 <html>
     <head>
         <meta charset="utf-8" />
-        <title>Titre de ma page</title>
+        <title>Minichat</title>
     </head>
     <body>
+    <?php
+      try
+      {
+          $bdd = new PDO('mysql:host=localhost;dbname=minichat;charset=utf8', 'root', 'melissa2000');
+      }
+      catch(Exception $e)
+      {
+              die('Erreur : '.$e->getMessage());
+      }
 
 
+      $req = $bdd->prepare('INSERT INTO minichat (nom, message) VALUES(?, ?)');
 
+      $req->execute(array($_POST['nom'], $_POST['message']));
 
+      $reponse = $bdd->query('SELECT nom, message FROM minichat ORDER BY id DESC LIMIT 0, 10');
+      while ($donnees = $reponse->fetch())
+      {
+          echo '<p><strong>' . htmlspecialchars($donnees['nom']) . '</strong> : ' . htmlspecialchars($donnees['message']) . '</p>';
+
+      }
+
+      $reponse->closeCursor();
+
+      header('Location: minichat.php');
+
+      ?>
     </body>
 </html>
